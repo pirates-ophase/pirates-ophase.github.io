460ef604474a7f9f4bcc8122ceb9daba00683b5d
Max Schweikart <hello@maxschweik.art>
2022-10-12 18:53:35 +0200

    add parallax effect to menu

--- components/Menu.tsx
+++ components/Menu.tsx
@@ -1,11 +1,30 @@
 import NextLink from 'next/link';
 
+import Parallax from 'parallax-js';
+import React, { useEffect, useRef } from 'react';
+import styles from '../styles/Menu.module.css'
+
 export const Menu = () => {
-  
-    return <nav>
-        <NextLink href={'/'}>Home</NextLink>
-        <NextLink href={'/crew'}>Crew</NextLink>
-        <NextLink href={'/plan'}>Wochenplan</NextLink>
-        <NextLink href={'/log'}>Logbuch</NextLink>
+    const parallaxSceneRef = useRef<HTMLElement>(null);
+
+    useEffect(() => { // maybe useLayoutEffect
+        if (parallaxSceneRef.current === null) throw new Error('missing scene');
+
+        const parallax = new Parallax(parallaxSceneRef.current, {
+            pointerEvents: true,
+        });
+
+        return function cleanup() {
+            parallax.destroy();
+        }
+    }, [/* this side effect is not dependent on any props */]);
+
+    return <nav className={styles.menu} ref={parallaxSceneRef}>
+        <div data-depth="0.2">
+            <NextLink href={'/'}>Home</NextLink>
+            <NextLink href={'/crew'}>Crew</NextLink>
+            <NextLink href={'/plan'}>Wochenplan</NextLink>
+            <NextLink href={'/log'}>Logbuch</NextLink>
+        </div>
     </nav>
 };
