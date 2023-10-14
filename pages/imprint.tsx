6a23b601bca9712d2d4d7b85267cb5f0f40b2f7c
PhilippSeidel00 <spam@philipp-seidel.de>
2023-10-14 11:24:36 +0200

    added email adress, billyboy and logo shoutout as well as disclaimer

--- pages/imprint.tsx
+++ pages/imprint.tsx
@@ -25,13 +25,28 @@ const Home: NextPage = () => {
           Pirates O-Phasengruppe <br></br>
           c/o AStA am KIT <br></br>
           Adenauerring 7 <br></br>
-          76133 Karlsruhe
+          76133 Karlsruhe <br></br>
+          <a href="mailto:hsg@asta-kit.de?subject=Pirates O-Phasengruppe">hsg@asta-kit.edu</a>
         </p>
 
         <p className={styles.description}>
           Wir freuen uns auch dieses Jahr wieder von unseren 
-          Sponsoren mit Nahrung und finanziellen Mitteln versorgt worden zu sein.
-          Vielen Dank an <NextLink href="https://www.andrena.de/">Andrena Objects</NextLink> und die <NextLink href="https://www.badische-backstub.de/">Badische Backstub</NextLink>.
+          Sponsoren mit Nahrung, Goodies und finanziellen Mitteln versorgt worden zu sein.
+          Vielen Dank dafür an <NextLink href="https://www.andrena.de/">Andrena Objects</NextLink>, <NextLink href="https://www.billy-boy.de/">Billy Boy</NextLink> und die <NextLink href="https://www.badische-backstub.de/">Badische Backstub</NextLink>.
+        </p>
+
+        <p className={styles.description}>
+          Vielen Dank auch an <NextLink href="https://www.linkedin.com/in/julie-elena-beck/">Julie Beck</NextLink> für unser diesjähriges Logo.
+        </p>
+
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+        <p>&nbsp;</p>
+
+        <p className={styles.disclaimer}>
+        Mit Urteil vom 12. Mai 1998 - 312 O 85/98 - "Haftung für Links" hat das Landgericht (LG) Hamburg entschieden, dass man durch das Setzen eines Links, die Inhalte der gelinkten Seite ggf. mit zu verantworten hat. Dies kann - so das LG - nur dadurch verhindert werden, dass man sich ausdrücklich von diesen Inhalten distanziert. Hiermit distanzieren wir uns ausdrücklich von den verlinkten Seiten.
         </p>
       </section>
     </Layout>
